5cc9d00e7e473467e201d7c764e34c93a480a95b
Bongshim <bongshime@gmail.com>
2022-07-02 15:08:58 +0100

    feat: added about and portfolio section

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# portfolio
+# Personal Portfolio
+
+## Pages
+- Index page (brief introduction)
+- About page (objectives as a developer)
+- Projects page (list of projects and kills)
+- Contact page (contact form)
+
+*All pages must include links to social media platforms: LinkedIn, GitHub, Twitter.*
+
---
+++ assets/JS/control.js
@@ -0,0 +1,45 @@
+const navMenu = document.getElementsByClassName("mb_nav"),
+  navToggle = document.getElementsByClassName("mb-menu"),
+  navClose = document.getElementsByClassName("uil-times"),
+  navLink = document.querySelectorAll(".nav__link");
+// When we click on the navToggle, we add the left menu class
+if (navToggle) {
+  navToggle[0].addEventListener("click", () => {
+    navMenu[0].classList.add("left");
+  });
+}
+
+// When we click on the navClose, we remove the left menu class
+if (navClose) {
+  navClose[0].addEventListener("click", () => {
+    navMenu[0].classList.remove("left");
+  });
+}
+
+// when we click on the navLink, we remove the left menu class
+function linkAction() {
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu[0].classList.remove("left");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+
+// Acordion skills section
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((h) => h.addEventListener("click", toggleSkills));
+
+
+
